3754c86c6992960e5580209ffee34cdf1e555fdc
Tomoyuki Saito <aocchoda@gmail.com>
2012-02-25 11:43:54 +0900

    1st arch min-rt-error

---
+++ simulator/sim/oc_simulate.c
@@ -0,0 +1,445 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <assert.h>
+#include <math.h>
+#include "sim.h"
+
+int32_t reg[REG_NUM];
+uint32_t freg[REG_NUM];
+uint32_t rom[ROM_NUM];
+uint32_t ram[RAM_NUM];
+uint32_t pc;
+uint32_t lr, tmplr;
+long long unsigned cnt;
+void to_bin(uint32_t);
+#define dump(x) fprintf(stderr, "%s: ", #x); to_bin(x);
+#define SIGN(x) (((x)&0x80000000)>>31)
+#define ELSE(x) ((x)&0x7fffffff)
+
+// define fetch functions ////////////////////
+DEF_ELE_GET(get_opcode, 26, 0x3f);		
+DEF_ELE_GET(get_rsi, 21, 0x1f);
+DEF_ELE_GET(get_rti, 16, 0x1f);
+DEF_ELE_GET(get_rdi, 11, 0x1f);
+DEF_ELE_GET(get_shamt, 6, 0x1f);
+DEF_ELE_GET(get_funct, 0, 0x3f);
+DEF_ELE_GET(get_target, 0, 0x3ffffff);
+int32_t get_imm(uint32_t ir) {
+	return (ir & (1 << 15)) ?
+		   ((0xffff<<16) | (ir & 0xffff)):
+		   (ir & 0xffff);
+}
+
+int simulate(char *sfile) {
+	uint32_t ir, heap_size;
+	int fd,ret,i;
+	uint8_t opcode, funct;
+	union {
+		uint32_t i;
+		float f;
+	} a, b, out, ans;
+	uint32_t sendbuf_count = 0;
+	uint32_t max_sendbuf_count = 0;
+	int fjlt_flag, fjlt_ans;
+	char c;
+#ifdef LST_FLAG
+	FILE *lst_fp;
+	char lst_name[1024];
+#endif
+	//uint32_t g1_min = 4*RAM_NUM, g2_max = 0;
+
+	fd = open(sfile, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "%s: No such file\n", sfile);
+		return 1;
+	}
+	ret = read(fd, rom, ROM_NUM*4);
+	close(fd);
+
+	lr = tmplr = cnt = pc = 0;
+	reg[1] = reg[31] = 4*(RAM_NUM-1);
+	fprintf(stderr, "%d\n", reg[1]);
+	reg[2] = 0;
+
+	heap_size = rom[0];
+	pc++;
+	for (i = 0; heap_size > 0; i++,pc++) {
+		ram[reg[2]/4] = rom[pc];
+		reg[2] += 4;
+		heap_size -= 4;
+	}
+
+#ifdef LST_FLAG
+	#undef DEBUG_FLAG
+	sprintf(lst_name, "%s.lst", sfile);
+	fprintf(stderr, "output %s\n", lst_name);
+	lst_fp = fopen(lst_name, "w");
+	fprintf(lst_fp, "#### %s\n\n", lst_name);
+#endif
+
+#ifdef DEBUG_FLAG
+	fprintf(stderr, "debugging %s\n", sfile);
+	debug_usage();
+	fprintf(stderr, "\n[Debug start]\n");
+#else
+	fprintf(stderr, "simulate %s\n", sfile);
+	fflush(stderr);
+#endif
+
+#ifdef STATS_FLAG
+	statistics(NULL,1);
+#endif
+	do{
+		
+		if (reg[1] < 0 || reg[2] < 0)  {
+			break;
+		}
+
+		//if (!(cnt % 13750)) {
+		if (!(cnt % 1611)) {
+		//if (!(cnt % 20625)) {
+			if (sendbuf_count >0) {
+				sendbuf_count--;
+			}
+		}
+
+		ir = rom[pc];
+		cnt++;
+#ifdef LST_FLAG
+		print_ir(ir, lst_fp);
+#endif
+
+#ifdef STATS_FLAG
+		statistics(stderr,0);
+#endif
+
+#ifdef DEBUG_FLAG
+		debug();
+#endif
+
+		opcode = get_opcode(ir);
+		funct = get_funct(ir);
+		pc++;
+		if (!(cnt % 100000000)) {
+			fprintf(stderr, ".");
+			fflush(stderr);
+		}
+   
+		switch(opcode){
+			case LD:
+				_GRD = ram[((_GRS + _GRT)/4)];
+				break;
+			case ST:
+				ram[((_GRS + _GRT)/4)] = _GRD;
+				break;
+			case LDI:
+				IF0_BREAK_S
+				_GRT = ram[((_GRS - _IMM)/4)];
+				break;
+			case STI:
+				ram[((_GRS - _IMM)/4)] = _GRT;
+				break;
+			case FLDI:
+				_FRT = ram[((_GRS - _IMM)/4)];
+				break;
+			case JNE:
+				if (_GRS != _GRT) {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+					pc += _IMM - 1;
+				} else {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+				}
+				break;
+			case ADDI:
+				IF0_BREAK_T
+				_GRT = _GRS + _IMM;
+				break;
+			case SLLI:
+				IF0_BREAK_T
+				_GRT = _GRS << _IMM;
+				break;
+			case JMP:
+				pc = get_target(ir);
+				break;
+			case FJLT:
+				a.i = _FRS;
+				b.i = _FRT;
+				if (((SIGN(a.i))==1) && ((SIGN(b.i))==0)) {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+					fjlt_flag = 1;
+					pc += _IMM - 1;
+				} else if (SIGN(a.i)==0 && SIGN(b.i)==1) {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+					fjlt_flag = 0;
+					pc = pc;
+				} else if (SIGN(a.i)==0 && SIGN(b.i)==0) {
+					if (ELSE(a.i) < ELSE(b.i)) {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+						fjlt_flag = 1;
+						pc += _IMM - 1;
+					} else {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+						fjlt_flag = 0;
+							pc = pc;
+					}
+				} else {
+					if (ELSE(a.i) > ELSE(b.i)) {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+						fjlt_flag = 1;
+						pc += _IMM - 1;
+					} else {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+					fjlt_flag = 0;
+							pc = pc;
+					}
+				}
+				if (a.f < b.f) {
+					//pc += _IMM - 1;
+					fjlt_ans = 1;
+				} else {
+					fjlt_ans = 0;
+				}
+				if (fjlt_flag != fjlt_ans) {
+					fprintf(stderr, "ans: %d, flag %d\n", fjlt_ans, fjlt_flag);
+					fprintf(stderr, "a: %f, b: %f\n", a.f, b.f);
+					fprintf(stderr, "sa: %d, sb: %d\n", SIGN(a.i), SIGN(b.i));
+					dump(a.i);
+					dump(b.i);
+				}
+				break;
+			case FSTI:
+				ram[((_GRS - _IMM)/4)] = _FRT;
+				break;
+			case SUBI:
+				IF0_BREAK_T
+				_GRT = _GRS - _IMM;
+				break;
+			case RETURN:
+				pc = lr;
+				reg[1] += 4;
+				lr = ram[reg[1]/4];
+				break;
+			case FJEQ:
+				a.i = _FRS;
+				b.i = _FRT;
+				if (a.f == b.f)  {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+					pc += _IMM - 1;
+				} else {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+				}
+				break;
+			case JLT:
+				if (_GRS < _GRT) {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+					pc += _IMM - 1;
+				} else {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+				}
+				break;
+			case CALL:
+				ram[reg[1]/4] = lr;
+				reg[1] -= 4;
+				lr = pc;
+				pc = get_target(ir);
+				break;
+			case SRLI:
+				IF0_BREAK_T
+				_GRT = _GRS >> _IMM;
+				break;
+			case JEQ:
+				if (_GRS == _GRT) {
+#ifdef CHECK_BRANCH
+					printf("t");
+#endif
+					pc += _IMM - 1;
+				} else {
+#ifdef CHECK_BRANCH
+					printf("f");
+#endif
+				}
+				break;
+			case MULI:
+				IF0_BREAK_T
+				_GRT = _GRS * _IMM;
+				break;
+
+			case MVHI: 
+				IF0_BREAK_S
+				_GRS = ((uint32_t) _IMM << 16) | (_GRS & 0xffff);
+				break;
+			case MVLO: 
+				IF0_BREAK_S
+				_GRS = (_GRS & (0xffff<<16)) | (_IMM & 0xffff);
+				break;
+			case DIVI:
+				IF0_BREAK_T
+				_GRT = _GRS / _IMM;
+				break;
+			case FPI:
+				switch(funct) {
+					case FMUL_F:
+						_FRD = _fmul(_FRS, _FRT);
+						break;
+					case FDIV_F:
+						a.i = _finv(_FRT);
+						_FRD = _fmul(_FRS, a.i);
+						break;
+					case FADD_F:
+						a.i = _FRS;
+						b.i = _FRT;
+						out.i = _fadd(_FRS, _FRT);
+						_FRD = out.i;
+
+						break;
+					case FSUB_F:
+						a.i = _FRS;
+						b.i = (_FRT >> 31) ? (_FRT & 0x7fffffff) : (_FRT | 0x80000000);
+						out.i = _fadd(_FRS, b.i);
+						_FRD = out.i;
+						
+						break;
+					case FABS_F:
+						_FRD = 0x7fffffff & _FRS;
+						break;
+					case FMOV_F:
+						_FRD = _FRS;
+						break;
+					case FNEG_F:
+						_FRD = (_FRS & (0x1 << 31)) ?
+										 (_FRS & 0x7fffffff) : // minus
+										 (_FRS | (0x1 << 31)) ; // plus
+						break;
+					case FSQRT_F:
+						_FRD = _fsqrt(_FRS);
+						break;
+					default: break;
+
+				}
+				break;
+
+
+			case SPECIAL:
+				switch(funct) {
+					case ADD_F: 
+						IF0_BREAK_D
+						_GRD = _GRS + _GRT;
+						break;
+					case NOR_F: 
+						IF0_BREAK_D
+						_GRD = ~(_GRS | _GRT);
+						break;
+					case CALLR_F:
+						ram[reg[1]/4] = lr;
+						reg[1] -= 4;
+						lr = pc;
+						pc = _GRS;
+						break;
+					case B_F:
+						pc = _GRS;
+						break;
+					case FLD_F:
+						_FRD = ram[(_GRS + _GRT)/4];
+						break;
+					case FST_F:
+						ram[(_GRS + _GRT)/4] = _FRD;
+						break;
+					case SUB_F:
+						IF0_BREAK_D
+						_GRD = _GRS - _GRT;
+						break;
+					case MUL_F:
+						IF0_BREAK_D
+						_GRD = _GRS * _GRT;
+						break;
+					//case DIV_F:
+						//IF0_BREAK_D
+						//_GRD = _GRS / _GRT;
+						//break;
+					case AND_F:
+						IF0_BREAK_D
+						_GRD = _GRS & _GRT;
+						break;
+					case OR_F:
+						IF0_BREAK_D
+						_GRD = _GRS | _GRT;
+						break;
+					case SLL_F:
+						IF0_BREAK_D
+						_GRD = _GRS << _GRT;
+						break;
+					case SRL_F:
+						IF0_BREAK_D
+						_GRD = _GRS >> _GRT;
+						break;
+					case HALT_F:
+						break;
+					default: break;		
+				}
+				break;
+			case IO:
+				switch(funct) {
+					case OUTPUT_F:
+						putchar(_GRS&0xff);
+						fflush(stdout);
+						sendbuf_count++;
+						max_sendbuf_count = (max_sendbuf_count > sendbuf_count) ?
+								max_sendbuf_count : sendbuf_count;
+						break;
+					case INPUT_F:
+						//ret = scanf("0x%02x", (char*)&c);
+						//if (ret==0) { fprintf(stderr,"input error\n"); }
+						c = getchar();
+						//fprintf(stderr, " debug input: 0x%08X\n", c);
+						IF0_BREAK_D
+						_GRD = c & 0xff;
+						break;
+					default: break;
+
+					}
+				break;
+			default	:	break;
+		}
+
+	} while(!((funct == HALT_F) && (opcode == SPECIAL)));
+	fprintf(stderr, "max_sendbuf: %d\n", max_sendbuf_count);
+
+#ifdef LST_FLAG
+	fclose(lst_fp);
+#endif
+	fprintf(stderr, "\nCPU Simulator Results\n");
+	fprintf(stderr, "cnt:%llu\n", cnt);
+	fflush(stderr);
+
+	return 0;
+} 
---
+++ simulator/sim/print_state.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <string.h>
+#include "sim.h"
+enum Print_Flags {
+ON,GC,REG,RAM,PRINT_FLAG_NUM
+};
+
+int __print_state(int init_flag, int argc, char **argv) {
+	static FILE *fp = NULL;
+	static int flag[PRINT_FLAG_NUM] = {0};
+	static const char *break_points[INST_NUM];
+	static int ram_s, ram_f, bpcnt = 0;
+	int i,bp;
+
+	if (init_flag) {
+	// Initialize /////////////////////////////////////////
+		for (i = 0; i < argc; i++) {
+			if (argv[i][0] == '-') {
+				if (strcmp(argv[i], "-g") == 0) {
+					// -g	// getchar each print_state
+					flag[ON] = flag[GC] = 1;
+					fp = stdout;
+				} else if (strcmp(argv[i], "-reg")==0) {
+					// -reg  // print all registers
+					flag[ON] = flag[REG] = 1; 
+				} else if (strcmp(argv[i], "-ram")==0) {
+					// -ram 0 100  // print ram
+					flag[ON] = flag[RAM] = 1; 
+					if ((sscanf(argv[++i], "%d", &ram_s) != 1) ||
+						(sscanf(argv[++i], "%d", &ram_f) != 1)) {
+						puts("USAGE: -ram [start num] ([end_num])");
+						return -1;
+					}
+				} else if (strcmp(argv[i], "-op")==0) {
+					// -op ld jmp jeq [END]// break point
+					flag[ON] = 1;
+					i++;
+					for (; i < argc;bpcnt++,i++) {
+						break_points[bpcnt] = argv[i];
+					}
+				} else if (strcmp(argv[i], "-o")==0) {
+					flag[ON] = 1;
+					fp = fopen(argv[++i], "w");
+				} else if (strcmp(argv[i], "-o1")==0) {
+					flag[ON] = 1;
+					fp = stdout;
+				} else if (strcmp(argv[i], "-o2")==0) {
+					flag[ON] = 1;
+					fp = stderr;
+				} else if (strcmp(argv[i], "-p")==0) {
+					flag[ON] = 1;
+				} else if (strcmp(argv[i], "-res")==0) {
+					flag[ON] = 1;
+					break_points[bpcnt++] = "halt";
+				} else {
+					fprintf(stderr, "useless option:%s\n",argv[i]);
+				}
+			}
+		}
+		if ((flag[ON] == 1) && (fp == NULL)) {
+			fp = fopen(SIM_LOG, "w");
+		}
+	} else {
+
+
+		if (flag[ON]) {
+			if (bpcnt > 0) {
+				for (i=0,bp=0; i < bpcnt; i++) {
+					if (strcmp(break_points[i], InstMap[get_opcode(rom[pc])]) == 0)
+						bp=1;
+				}
+				if (bp==0) 
+					return 0;
+			}
+
+			
+			print_ir(rom[pc/4],fp);
+
+			if (flag[REG]) {
+				for (i = 0; i < 32; i++) {
+					fprintf(fp, "g%d:%x\n", i, reg[i]);
+				}
+				/*
+				fprintf(fp, "g0:%X g1:%X g2:%X g3:%X g4:%X g5:%X g6:%X g7:%X\n",
+							reg[0],reg[1],reg[2],reg[3],reg[4],reg[5],reg[6],reg[7]);
+				fprintf(fp, "g8:%X g9:%X g10:%X g11:%X g12:%X g13:%X g14:%X g15:%X\n",
+							reg[8],reg[9],reg[10],reg[11],reg[12],reg[13],reg[14],reg[15]);
+				fprintf(fp, "g16:%X g17:%X g18:%X g19:%X g20:%X g21:%X g22:%X g23:%X\n",
+							reg[16],reg[17],reg[18],reg[19],reg[20],reg[21],reg[22],reg[23]);
+				fprintf(fp, "g24:%X g25:%X g26:%X g27:%X g28:%X g29:%X g30:%X g31:%X\n",
+							reg[24],reg[25],reg[26],reg[27],reg[28],reg[29],reg[30],reg[31]);
+				fflush(fp);
+				fprintf(fp, "f0:%f f1:%f f2:%f f3:%f f4:%f f5:%f f6:%f f7:%f\n",
+							(float)freg[0],(float)freg[1],(float)freg[2],(float)freg[3],(float)freg[4],(float)freg[5],(float)freg[6],(float)freg[7]);
+				fprintf(fp, "f8:%f f9:%f f10:%f f11:%f f12:%f f13:%f f14:%f f15:%f\n",
+							(float)freg[8],(float)freg[9],(float)freg[10],(float)freg[11],(float)freg[12],(float)freg[13],(float)freg[14],(float)freg[15]);
+				fprintf(fp, "f16:%f f17:%f f18:%f f19:%f f20:%f f21:%f f22:%f f23:%f\n",
+							(float)freg[16],(float)freg[17],(float)freg[18],(float)freg[19],(float)freg[20],(float)freg[21],(float)freg[22],(float)freg[23]);
+				fprintf(fp, "f24:%f f25:%f f26:%f f27:%f f28:%f f29:%f f30:%f f31:%f\n",
+							(float)freg[24],(float)freg[25],(float)freg[26],(float)freg[27],(float)freg[28],(float)freg[29],(float)freg[30],(float)freg[31]);
+				fflush(fp);
+				*/
+			}
+			if (flag[RAM]) {
+				for (i = ram_s; i < ram_f; i++) {
+					if (!((i-ram_s)%8))
+						fprintf(fp, "\n");
+					fprintf(fp, "ram%d:%X ", i,ram[i]);
+				}
+				fprintf(fp, "\n");
+				fflush(fp);
+			}
+			if (flag[GC])
+				getchar();
+		}
+	}
+
+	return 0;
+}
